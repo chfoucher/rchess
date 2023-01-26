3b31d624b3e840a223a967290d80f1647b18b62c
Christophe Foucher <pele.foucher@free.fr>
2023-01-26 11:47:28 +0100

    Suppression des composants inutiles

--- src/composants/App.js
+++ src/composants/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import Banner from './Banner';
-import Cart from './Cart';
-import ShoppingList from './ShoppingList';
 import Game from './Game';
 
 function App() {
   return (
   <React.Fragment>
-    <Banner />
-    <Cart />
-    <ShoppingList />
     <Game />
   </React.Fragment>
   );
