a8248eb919cd977a9b62d179ce8a56af97da5baf
Christophe Foucher <pele.foucher@free.fr>
2023-01-31 10:53:37 +0100

    Ajout de descriptions aux tests.

--- src/composants/Game.test.js
+++ src/composants/Game.test.js
@@ -1,17 +1,25 @@
 import { initBoard, initBoardLayout } from "./Game";
 
-test('Fonction initBoard', () => {
+describe('Fonction initBoard', () => {
     const board = initBoard();
-    expect(board.length).toBe(8);
-    expect(board[0].length).toBe(8);
-    expect(board[0][0]).toEqual({ r:0, c:0, black: false });
-    expect(board[1][0]).toEqual({ r:1, c:0, black: true });
+    it('should return an 8x8 array', () => {
+        expect(board.length).toBe(8);
+        expect(board[0].length).toBe(8);
+    })
+    it('should alternate black and white cells', () => {
+        expect(board[0][0]).toEqual({ r:0, c:0, black: false });
+        expect(board[1][0]).toEqual({ r:1, c:0, black: true });
+    })
 });
 
-test('Fonction initBoardLayout', () => {
+describe('Fonction initBoardLayout', () => {
     const board = initBoardLayout(initBoard());
-    expect(board.length).toBe(8);
-    expect(board[0].length).toBe(8);
-    expect(board[0][0]).toEqual({ r:0, c:0, black: false, piece: { type: 2, couleur: "noir" } });
-    expect(board[2][0]).toEqual({ r:2, c:0, black: false, piece: undefined });
+    it('should return an 8x8 array', () => {
+        expect(board.length).toBe(8);
+        expect(board[0].length).toBe(8);
+    })
+    it('should setup pieces for a new game', () => {
+        expect(board[0][0]).toEqual({ r:0, c:0, black: false, piece: { type: 2, couleur: "noir" } });
+        expect(board[2][0]).toEqual({ r:2, c:0, black: false, piece: undefined });
+    })
 });
